71e42bb18bd9c14872a2d5a80b8160d26f44a5b9
yasintayusniawati <37019733+yasintayusniawati@users.noreply.github.com>
2020-03-02 12:20:21 +0700

    tugas2

---
+++ Tugas 2/udp_simple.py
@@ -0,0 +1,8 @@
+
+import socket
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 5006
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(bytes('Yasinta'.encode()),(TARGET_IP,TARGET_PORT))
