68e8153c30daf236415111802b9407b27b215c5e
yasintayusniawati <37019733+yasintayusniawati@users.noreply.github.com>
2020-03-02 12:49:39 +0700

    gambar

--- Tugas 2/udp_simple.py
+++ Tugas 2/udp_simple.py
@@ -5,4 +5,4 @@ TARGET_IP = "127.0.0.1"
 TARGET_PORT = 5006
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes('Yasinta'.encode()),(TARGET_IP,TARGET_PORT))
+sock.sendto(bytes('Yasinta-ProgjarC'.encode()),(TARGET_IP,TARGET_PORT))
