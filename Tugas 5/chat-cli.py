ac99582d18a06f19d3f551497a4c5f6a53736db9
yasintayusniawati <37019733+yasintayusniawati@users.noreply.github.com>
2020-04-06 10:55:17 +0700

    tugas 5

---
+++ Tugas 5/chat-cli.py
@@ -0,0 +1,129 @@
+import socket
+import os
+import json
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 8889
+
+
+class ChatClient:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (TARGET_IP,TARGET_PORT)
+        self.sock.connect(self.server_address)
+        self.tokenid=""
+    def proses(self,cmdline):
+        j=cmdline.split(" ")
+        try:
+            command=j[0].strip()
+            if (command=='auth'):
+                username=j[1].strip()
+                password=j[2].strip()
+                return self.login(username,password)
+            elif (command=='send'):
+                usernameto = j[1].strip()
+                message=""
+                for w in j[2:]:
+                   message="{} {}" . format(message,w)
+                return self.sendmessage(usernameto,message)
+            elif (command=='inbox'):
+                return self.inbox()
+            elif (command=='list_user'):
+                return self.list_user()
+            elif (command=='user_aktif'):
+                return self.user_aktif()
+            elif (command=='logout'):
+                return self.logout()
+            else:
+                return "*Maaf, command tidak benar"
+        except IndexError:
+                return "-Maaf, command tidak benar"
+    def sendstring(self,string):
+        try:
+            self.sock.sendall(string.encode())
+            receivemsg = ""
+            while True:
+                data = self.sock.recv(64)
+                print("diterima dari server",data)
+                if (data):
+                    receivemsg = "{}{}" . format(receivemsg,data.decode())  #data harus didecode agar dapat di operasikan dalam bentuk string
+                    if receivemsg[-4:]=='\r\n\r\n':
+                        print("end of string")
+                        return json.loads(receivemsg)
+        except:
+            self.sock.close()
+            return { 'status' : 'ERROR', 'message' : 'Gagal'}
+    def login(self,username,password):
+        string="auth {} {} \r\n" . format(username,password)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            self.tokenid=result['tokenid']
+            return "username {} logged in, token {} " .format(username,self.tokenid)
+        else:
+            return "Error, {}" . format(result['message'])
+    def sendmessage(self,usernameto="xxx",message="xxx"):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="send {} {} {} \r\n" . format(self.tokenid,usernameto,message)
+        print(string)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "message sent to {}" . format(usernameto)
+        else:
+            return "Error, {}" . format(result['message'])
+    def inbox(self):
+        if (self.tokenid==""):
+            return "Error, not authorized"
+        string="inbox {} \r\n" . format(self.tokenid)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            return "{}" . format(json.dumps(result['messages']))
+        else:
+            return "Error, {}" . format(result['message'])
+    def list_user(self):
+        if(self.tokenid==""):
+            return "Error, not authorized"
+        string="list_user {} \r\n".format(self.tokenid)
+
+        result = self.sendstring(string)
+        
+        if result['status']=='OK':
+            return "{}".format(json.dumps(result['list_user']))
+        else:
+            return "Error, {}".format(result['message'])
+
+    def user_aktif(self):
+        if (self.tokenid==""):
+            return "Error"
+        string = "user_aktif {} \r\n" .format(self.tokenid)
+        hasil = self.sendstring(string)
+        if hasil['status'] =='OK':
+            return "online user : {}\n" .format(json.dumps(hasil['message']))
+        else:
+            return "Error, {}" .format(hasil['message'])
+
+    def logout(self):
+        if(self.tokenid==""):
+            return "Opps, silahakan login terlebih dahulu!"
+
+        string = "logout {} \r\n" .format(self.tokenid)
+        hasil = self.sendstring(string)
+        if hasil['status'] =='OK':
+            self.tokenid=""
+            return "message : {}\n" .format(json.dumps(hasil['message']))
+        else:
+            return "Error, {}" .format(hasil['message'])
+            
+        return "Berhasil Logout"
+
+
+
+if __name__=="__main__":
+    cc = ChatClient()
+    print("Command :\n1.Authentication : auth [username] [password]\n" + 
+    "2.Kirim pesan : send [username pengirim] [isi pesan]\n" +
+    "3.Logout : logout\n" + 
+    "4.Melihat semua user : list_user\n")
+    while True:
+        cmdline = input("Command {}:" . format(cc.tokenid))
+        print(cc.proses(cmdline))
---
+++ Tugas 5/chat.py
@@ -0,0 +1,138 @@
+import sys
+import os
+import json
+import uuid
+import logging
+from queue import  Queue
+
+class Chat:
+	def __init__(self):
+		self.sessions={}
+		self.users = {}
+		self.users['messi']={ 'nama': 'Lionel Messi', 'negara': 'Argentina', 'password': 'surabaya', 'incoming' : {}, 'outgoing': {}}
+		self.users['henderson']={ 'nama': 'Jordan Henderson', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+		self.users['lineker']={ 'nama': 'Gary Lineker', 'negara': 'Inggris', 'password': 'surabaya','incoming': {}, 'outgoing':{}}
+	def proses(self,data):
+		j=data.split(" ")
+		try:
+			command=j[0].strip()
+			if (command=='auth'):
+				username=j[1].strip()
+				password=j[2].strip()
+				logging.warning("AUTH: auth {} {}" . format(username,password))
+				return self.autentikasi_user(username,password)
+			elif (command=='send'):
+				sessionid = j[1].strip()
+				usernameto = j[2].strip()
+				message=""
+				for w in j[3:]:
+					message="{} {}" . format(message,w)
+				usernamefrom = self.sessions[sessionid]['username']
+				logging.warning("SEND: session {} send message from {} to {}" . format(sessionid, usernamefrom,usernameto))
+				return self.send_message(sessionid,usernamefrom,usernameto,message)
+			elif (command=='inbox'):
+				sessionid = j[1].strip()
+				username = self.sessions[sessionid]['username']
+				logging.warning("INBOX: {}" . format(sessionid))
+				return self.get_inbox(username)
+			elif (command == 'list_user'):
+				sessionid = j[1].strip()
+				logging.warning("LIST USER: proses list semua user")
+				return self.get_list_user()
+			elif (command == 'user_aktif'):
+				sessionid = j[1].strip()
+				logging.warning("USER AKTIF: proses list user yang aktif")
+				return self.get_user_aktif()
+
+			elif (command == 'logout'):
+				sessionid = j[1].strip()
+				logging.warning("LOGOUT: proses logout user")
+				return self.get_logout(sessionid)
+
+				
+			else:
+				return {'status': 'ERROR', 'message': '**Protocol Tidak Benar'}
+		except KeyError:
+			return { 'status': 'ERROR', 'message' : 'Informasi tidak ditemukan'}
+		except IndexError:
+			return {'status': 'ERROR', 'message': '--Protocol Tidak Benar'}
+
+	def autentikasi_user(self,username,password):
+		if (username not in self.users):
+			return { 'status': 'ERROR', 'message': 'User Tidak Ada' }
+		if (self.users[username]['password']!= password):
+			return { 'status': 'ERROR', 'message': 'Password Salah' }
+		tokenid = str(uuid.uuid4()) 
+		self.sessions[tokenid]={ 'username': username, 'userdetail':self.users[username]}
+		return { 'status': 'OK', 'tokenid': tokenid }
+	def get_user(self,username):
+		if (username not in self.users):
+			return False
+		return self.users[username]
+	def send_message(self,sessionid,username_from,username_dest,message):
+		if (sessionid not in self.sessions):
+			return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+		s_fr = self.get_user(username_from)
+		s_to = self.get_user(username_dest)
+		
+		if (s_fr==False or s_to==False):
+			return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+
+		message = { 'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message }
+		outqueue_sender = s_fr['outgoing']
+		inqueue_receiver = s_to['incoming']
+		try:	
+			outqueue_sender[username_from].put(message)
+		except KeyError:
+			outqueue_sender[username_from]=Queue()
+			outqueue_sender[username_from].put(message)
+		try:
+			inqueue_receiver[username_from].put(message)
+		except KeyError:
+			inqueue_receiver[username_from]=Queue()
+			inqueue_receiver[username_from].put(message)
+		return {'status': 'OK', 'message': 'Message Sent'}
+
+	def get_inbox(self,username):
+		s_fr = self.get_user(username)
+		incoming = s_fr['incoming']
+		msgs={}
+		for users in incoming:
+			msgs[users]=[]
+			while not incoming[users].empty():
+				msgs[users].append(s_fr['incoming'][users].get_nowait())
+			
+		return {'status': 'OK', 'messages': msgs}
+	
+	def get_user_aktif(self):
+		user = " ".join(self.sessions.keys())
+		arr_user = user.split(" ")
+		user_aktif = ""
+		for i in arr_user:
+			user_aktif = user_aktif + self.sessions[i]['username'] + ", "
+		return {'status' : 'OK', 'message': '{}' .format(user_aktif)} 
+
+	def get_list_user(self):
+		return {'status': 'OK', 'list_user': list(self.users.keys())}
+	
+	def get_logout(self,sessionid):
+		if (sessionid not in self.sessions):
+			return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+		del self.sessions[sessionid]
+		return {'status': 'OK', 'message': 'Berhasil Logout'}
+
+if __name__=="__main__":
+	j = Chat()
+	i = 0
+	print(j.get_list_user())
+
+
+
+
+
+
+
+
+
+
+
